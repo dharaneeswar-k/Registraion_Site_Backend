e38a50767433441fdaddb63244595868c56131f8
DHARANEESWAR K <dharaneeswarkb@gmail.com>
2025-04-19 19:25:36 +0530

    Update Registration.js

--- models/Registration.js
+++ models/Registration.js
@@ -4,8 +4,8 @@ const registrationSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
-  gender: { type: String, required: true },
-  age: { type: Number, required: true },
+  qualification: { type: String, required: true },
+  schoolOrCollegeName: { type: String, required: true },
   paymentScreenshot: { type: String, default: '' },
   status: { type: String, default: 'pending' },
   registeredAt: { type: Date, default: Date.now }
